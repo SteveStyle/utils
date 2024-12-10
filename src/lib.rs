627ef9ea9f676be0773b8774679d46b3826de2a2
SteveStyle <stephenmor@gmail.com>
2024-12-10 13:11:56 +0000

    updated comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-/// This module contains utility functions that are used in the project.
 /// To use this crate, add `utils = { path = "utils" }` to the dependencies in Cargo.toml.
-/// Alternatively, use the git repository by adding `utils = { git = ..."
+/// Alternatively, use the git repository by adding 'utils = { git = "https://github.com/SteveStyle/utils.git" }'
 pub mod timer;
